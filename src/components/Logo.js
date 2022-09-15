e8f414967193a698ab0055faa8fe89d493fb4129
jaspreet12345678 <jaspreet.mangoit@gmail.com>
2022-09-15 10:04:39 +0530

    submit

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import { Box, Image, Text } from "@chakra-ui/react";
 export default function Logo(props) {
   return (
     <Box {...props}>
-      <Image src="http://3.95.161.176/assets/images/logo.png" />
+      <Image src="logo.png" />
        {/* <Text fontSize="lg" fontWeight="bold">
         Logo
       </Text> */}
